95680c9345cdf32179fe6bb7530c0d7b0d8e94e4
Ryan <Ryan@EB-B106-Row4-1.et.byu.edu>
2019-03-14 14:09:36 -0600

    PlayerState has to be initialized with an integer defining the player number. Updated change.m to have more explanatory comments.

--- PlayerState.m
+++ PlayerState.m
@@ -4,28 +4,28 @@ classdef PlayerState
     % player, including the number of actions, buys, and +coins they have.
     
     properties
-        player      % int
-        actions;    % int
-        buys;       % int
-        coins;      % int
-        hand        % tuple in Python, maybe a character array?
-        drawpile    % tuple in Python, maybe a character array?
-        discard     % tuple in Python, maybe a character array?
-        tableau     % tuple in Python, maybe a character array?
+        player      = 0;  % int
+        actions     = 0;  % int
+        buys        = 0;  % int
+        coins       = 0;  % int
+        hand        = []; % tuple in Python, maybe a character array?
+        drawpile    = []; % tuple in Python, maybe a character array?
+        discard     = []; % tuple in Python, maybe a character array?
+        tableau     = []; % tuple in Python, maybe a character array?
     end
     
     methods
-        function obj = PlayerState(player,actions,buys,coins,hand,drawpile,discard,tableau)
+        function obj = PlayerState(player)
             %PLAYERSTATE Construct an instance of this class
             %   Detailed explanation goes here
             obj.player = player;
-            obj.actions = actions;
-            obj.buys = buys;
-            obj.coins = coins;
-            obj.hand = hand;
-            obj.drawpile = drawpile;
-            obj.discard = discard;
-            obj.tableau = tableau;
+%             obj.actions = actions;
+%             obj.buys = buys;
+%             obj.coins = coins;
+%             obj.hand = hand;
+%             obj.drawpile = drawpile;
+%             obj.discard = discard;
+%             obj.tableau = tableau;
         end
         
     end
