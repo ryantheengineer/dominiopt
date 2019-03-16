2b9256f42e81ab09b5d52e85c1297a91f831af1a
ryantheengineer <ryan.larson.wa@gmail.com>
2019-03-16 16:48:58 -0600

    Simplified classes by removing specific card classes. Run cardlist.m to initialize cards available for use. Run initialize_drawpile function after cardlist.m after creating a PlayerState object to get an initial drawpile, made up of 7 coppers and 3 estates, and shuffled so the first hand is ready to be drawn.

--- Card.m
+++ Card.m
@@ -1,29 +1,25 @@
-classdef Card
+classdef Card < handle
     %UNTITLED3 Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-%         Property1
-        name
-        cost
-        treasure
-        vp
-        coins
-        cards
-        actions
-        buys
-%         potionCost
-%         effect
-        isAttack
-        isDefense
-%         reaction
-%         duration
+        name = [];
+        cost = 0;
+        treasure = 0;
+        vp = 0;
+        coins = 0;
+        cards = 0;
+        actions = 0;
+        buys = 0;
+        effect = [];
+        isAttack = false;
+        isDefense = false;
     end
     
     methods
         % Constructor method for Card class
-        function obj = Card(name,cost,treasure,coins,cards,actions,buys,...
-                isAttack,isDefense)
+        function obj = Card(name,cost,treasure,vp,coins,cards,actions,buys,...
+                effect,isAttack,isDefense)
             %UNTITLED3 Construct an instance of this class
             %   Detailed explanation goes here
             
@@ -36,6 +32,7 @@ classdef Card
                 obj.cards = cards;
                 obj.actions = actions;
                 obj.buys = buys;
+                obj.effect = effect;
                 obj.isAttack = isAttack;
                 obj.isDefense = isDefense;
             else
@@ -43,12 +40,12 @@ classdef Card
             end
         end
         
-        % Need method for implementing actions, card draws, or buys
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+%         % Need method for implementing actions, card draws, or buys
+%         function outputArg = method1(obj,inputArg)
+%             %METHOD1 Summary of this method goes here
+%             %   Detailed explanation goes here
+%             outputArg = obj.Property1 + inputArg;
+%         end
     end
 end
 
--- PlayerState.m
+++ PlayerState.m
@@ -4,33 +4,93 @@ classdef PlayerState < handle
     % player, including the number of actions, buys, and +coins they have.
     
     properties
-        player      = 0;  % int
+        player;           % int
         actions     = 0;  % int
-        buys        = 0;  % int
+        buys        = 1;  % int
         coins       = 0;  % int
         hand        = []; % tuple in Python, maybe a character array?
         drawpile    = []; % tuple in Python, maybe a character array?
         discard     = []; % tuple in Python, maybe a character array?
-        tableau     = []; % tuple in Python, maybe a character array?
+        tableau     = []; % tuple in Python, maybe a character array?       
     end
     
     methods
         function obj = PlayerState(player)
             %PLAYERSTATE Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.player = player;
-%             obj.actions = actions;
-%             obj.buys = buys;
-%             obj.coins = coins;
-%             obj.hand = hand;
-%             obj.drawpile = drawpile;
-%             obj.discard = discard;
-%             obj.tableau = tableau;
+            obj.player = player;    % Initialize with an integer that indicates the player number
         end
         
-        function change(obj,delta_actions)
+        
+        % Change the currently available number of actions, buys, and coins
+        function change(obj,delta_actions,delta_buys,delta_coins)
+            % Change the number of actions, buys, cards, or coins available
+            % on this turn.
             obj.actions = obj.actions + delta_actions;
+            obj.buys = obj.buys + delta_buys;
+            obj.coins = obj.coins + delta_coins;
         end
+        
+        function [totalcards] = all_cards(obj)
+            totalcards = obj.hand + obj.tableau + obj.drawpile + obj.discard;
+        end
+        
+        function [handsize] = hand_size(obj)
+            handsize = length(obj.hand);
+        end
+        
+        function gain_cards(obj,cards)
+            obj.discard = obj.discard + cards;
+        end
+        
+        function [buysleft] = buyable(obj)
+            % Can this hand still buy a card?
+            buysleft = obj.buys > 0;
+        end
+        
+        
+        function initialize_drawpile(obj,firstcards)
+            % Initialize with 7 Copper and 3 Estate cards. firstcards must
+            % be a 1x2 array with copper first and estate second.
+            drawpile_0 = repelem(firstcards,[7,3]);
+            
+            % Randomly shuffle the cards so they are ready to be drawn
+            n = numel(drawpile_0);
+            ii = randperm(n);
+            drawpile_0 = drawpile_0(ii);
+            obj.drawpile = drawpile_0;
+            
+        end
+        
+        
+        
+        
+%         function isactionable = actionable(obj)
+%             % Are there any actions left to take with this hand?
+%             if obj.actions > 0 && 
+%             end
+%         end
+        
+%         NEED TO FIGURE OUT THIS AND PLAY_ACTION FUNCTIONS
+%         function play_card(obj,card)
+%             % Play a card from the hand into the tableau.
+%             % Decreasing the number of actions available is handled in
+%             % play_action function
+            
+            
+        
+%         NEED TO UNDERSTAND HOW DRAW FUNCTION WORKS MORE BEFORE
+%         IMPLEMENTING
+%         function draw(obj,n)
+%             % Returns a new PlayerState in which n cards have been drawn
+%             % (shuffling if necessary)
+%             if length(obj.drawpile) >= n
+%                 obj.hand
+        
+%         NEED TO MAKE THIS SYNTAX MORE MATLAB-FRIENDLY
+%         function totalcoins = hand_value(obj)
+%             % How many coins can the player spend?
+%             totalcoins = obj.coins + sum(card.treasure for card in obj.hand)
+%             end
         
     end
     
