05d44104a302f6b14e88c9b2fc00b8edf6e07ea1
Ryan <Ryan@EB-B106-Row4-1.et.byu.edu>
2019-03-21 13:57:10 -0600

    Updating testing.m

--- testing.m
+++ testing.m
@@ -11,7 +11,17 @@ cardlist;
 player1 = Player(1);
 player1.initialize(firstcards);
 
-player1
+player2 = Player(2);
+player2.initialize(firstcards);
+
+cards = [province duchy estate curse gold silver copper];
+
+[gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority] = random_strategy(cards);
+strategy1 = Strategy(cards,gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority);
+
+[gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority] = random_strategy(cards);
+strategy2 = Strategy(cards,gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority);
+
 
 % player1.draw(5);
 showcards(player1);
