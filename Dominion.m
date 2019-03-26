d1aa7536c3f49651b410e07a7c1aa90e8d00d394
ryantheengineer <ryan.larson.wa@gmail.com>
2019-03-25 21:32:47 -0600

    Dominion.m function runs a certain number of games with a chosen set of strategies. testdrive.m is a testing driver for setting up random inputs and comparing the outputs

--- Dominion.m
+++ Dominion.m
@@ -1,13 +1,32 @@
-function [avg_score_margin] = Dominion(ngames,players,strategies,cards)
+function [avg_score_margin] = Dominion(ngames,players,strategies,cards,firstcards)
     %DOMINION: Create a game and simulate it ngames times, and output the
     %average score margin between the target player (index called out by
     %target_player) and the highest-scoring other player
     numplayers = length(players);
-    scores = zeros(ngames,numplayers);
+    Scores = zeros(ngames,numplayers);
+    Margins = zeros(ngames,(numplayers-1));
+    maxMargins = zeros(ngames,1);
+    
+    % Load a Game object with the players, strategies, and cards
+    game = Game(players,strategies,cards);
+    
     
     for i = 1:ngames
-        
+        game.play_game(firstcards);
+        game.get_scores;
+        Scores(i,:) = game.scores;
+        Margins(i,:) = game.margins;
+        if numplayers == 2
+            maxMargins(i) = Margins(i);
+        else
+            maxMargins(i) = max(Margins(i,2:end));
+        end
+    end
     
+    % Calculate the average score margin for the number of simulations
+    % performed
+    
+    avg_score_margin = mean(maxMargins);
     
 end
 
---
+++ testdrive.m
@@ -0,0 +1,32 @@
+% Test drive creating a set of players and strategies, and then running n
+% games to see how good strategy 1 is.
+
+clear;
+
+%% Set up cards
+cardlist;
+cards = [province duchy estate gold silver copper village woodcutter smithy festival market laboratory chapel cellar moat harbinger];
+actioncards = [village woodcutter smithy festival market laboratory chapel cellar moat harbinger];
+
+%% Set up strategies
+[gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority] = random_strategy(cards);
+strategy1 = Strategy(cards,gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority);
+
+[gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority] = random_strategy(cards);
+strategy2 = Strategy(cards,gain_priority,gain_cutoffs,play_priority,discard_priority,trash_priority);
+
+strategies = [strategy1,strategy2];
+
+%% Set up players
+player1 = Player(1);
+player2 = Player(2);
+
+players = [player1,player2];
+
+%% Set up and run game simulations
+
+ngames = 100;
+avg_score_margin = Dominion(ngames,players,strategies,cards,firstcards);
+
+str = sprintf('Avg score margin: %0.2f',avg_score_margin);
+disp(str);
