079badd9102d017efc29185bb534613695a8e797
ryantheengineer <ryan.larson.wa@gmail.com>
2019-04-22 16:15:57 -0600

    Updating remote to make sure code works for more than 2 players

--- Dominiopt.m
+++ Dominiopt.m
@@ -5,23 +5,23 @@ function [avg_score_margin] = Dominiopt(gain_priority,gain_cutoffs,play_priority
 % players, and infers the total number of players from the number of
 % strategies fed in.
     ngames = 50;
-    numplayers = 2;
+    numplayers = 3;
     
     player1 = Player(1);
     player2 = Player(2);
-%     player3 = Player(3);
+    player3 = Player(3);
 %     player4 = Player(4);
     
-    players = [player1,player2]; % Change this to alter the number of players
+    players = [player1,player2,player3]; % Change this to alter the number of players
 %     assert(length(players) == numplayers);
     
     % Create strategies
     strategy1 = Strategy(gain_priority,gain_cutoffs,play_priority,trash_priority);
     strategy2 = chooseOpponent('BigMoney');
-%     strategy3 = chooseOpponent('BigSmithy');
+    strategy3 = chooseOpponent('BigSmithy');
 %     strategy4 = chooseOpponent('DoubleWitch');
     
-    strategies = [strategy1,strategy2];
+    strategies = [strategy1,strategy2,strategy3];
     assert(length(strategies) == numplayers);
     
     avg_score_margin = Dominion(ngames,players,strategies,cards,firstcards);
